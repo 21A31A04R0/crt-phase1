0b9589ddbe688911ea1bc58b3accbcc3a8fa196a
21A31A04R0 <146089163+21A31A04R0@users.noreply.github.com>
2024-03-23 15:46:37 +0530

    Update MINI PROJECT 3.py

--- MINI PROJECT 3.py
+++ MINI PROJECT 3.py
@@ -1,29 +1,53 @@
 class Carshowroom:
-    def _init_(self):
+    def init(self):
         self.company_name = ""
         self.model_name = ""
-        self.showroom_price = 0
+        self.showroom_price=""
 
     def Company(self, company_name):
-        self.company_name = company_name
-        print("Welcome to", self.company_name, "company")
+        
+            self.company_name = company_name
+            print("Welcome to", self.company_name, "company")
 
     def Model(self):
-        if self.company_name == "Hyundai":
-            models = ["Creta", "Venue", "I20"]
-        elif self.company_name == "Renault":
-            models = ["Kwid", "Triber", "Kiger"]
-        elif self.company_name == "MG":
-            models = ["ZS EV", "Astor", "Comet EV"]
+            if self.company_name == "Hyundai":
+                 models = ["Creta", "Venue", "I20"]
+
+            elif self.company_name == "Renault":
+                 models = ["Kwid", "Triber", "kiger"]
+            
+            elif self.company_name == "MG":
+                 models = ["ZS EV", "Astor", "Comet EV"]
+                
+                
+            else:
+               print("Invalid company name")
+               return
+            print("Models available for", self.company_name, ":", models)  
+            self.model_name = input("Select a model: ")
+
+    def Price(self):
+        if self.company_name=="Hyundai" and self.model_name=="Creta":
+            self.showroom_price=25000
+        elif self.company_name=="Hyundai" and self.model_name=="Venue":
+            self.showroom_price=26000
+        elif self.company_name=="Hyundai" and self.model_name=="I20":
+             self.showroom_price=31000
+        elif self.company_name=="Renault" and self.model_name=="Kwid":
+            self.showroom_price=27000
+        elif self.company_name=="Renault" and self.model_name=="Triber":
+            self.showroom_price=28000
+        elif self.company_name=="Renault" and self.model_name=="Kiger":
+             self.showroom_price=32000
+        elif self.company_name=="MG"and self.model_name=="ZS EV":
+            self.showroom_price=29000
+        elif self.company_name=="MG" and self.model_name=="Astor":
+            self.showroom_price=30000
+        elif self.company_name=="MG" and self.model_name=="comet EV":
+             self.showroom_price=33000
         else:
-            print("Invalid company name")
+            print("invalid")
             return
-
-        print("Models available for", self.company_name, ":", models)
-        self.model_name = input("Select a model: ")
-
-    def Price(self, showroom_price):
-        self.showroom_price = showroom_price
         gst_tax = 0.05 * self.showroom_price
         cgst_tax = 0.05 * self.showroom_price
         road_price = self.showroom_price + gst_tax + cgst_tax
@@ -33,5 +57,5 @@ car_showroom = Carshowroom()
 comp = input("Enter Company Name")
 car_showroom.Company(comp)
 car_showroom.Model()
-car_showroom.Price(25000)
-
+car_showroom.Price()
+        
